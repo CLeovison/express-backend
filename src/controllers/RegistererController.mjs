ba713406255eb96daa749837ed0d84b32ecf223d
Leovison <cleovison@gmail.com>
2024-04-17 16:41:33 +0800

    Added A Comments on Controllers

--- src/controllers/RegistererController.mjs
+++ src/controllers/RegistererController.mjs
@@ -5,8 +5,12 @@ export const RegisterController = {
     //The reason why the "New" keyword was use is because whenever the their are new input then
     //It will automatically create a new body for the Register
     const newRegister = new Register(req.body);
-
+    //Then the user input will be save in the database
     try {
+    const savedRegister = await newRegister.save();
+    
+    //If the Register is sucessful, send back the 201 status code and post body
+    if(savedRegister) res.status(201).send(newRegister)
     } catch (err) {
       console.log(err);
     }
