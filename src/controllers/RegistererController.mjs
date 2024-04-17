fd6f21f61da8023d3b72f3d40dd452064816dea2
Leovison <cleovison@gmail.com>
2024-04-17 14:20:20 +0800

    Added A Comment on The Create Register in Controller

--- src/controllers/RegistererController.mjs
+++ src/controllers/RegistererController.mjs
@@ -1,6 +1,18 @@
 import Register from "../model/User/Register.mjs";
 
 export const RegisterController = {
-  createRegister: async (req, res) => {},
-  getRegister: async (req, res) => {},
+  createRegister: async (req, res) => {
+    //The reason why the "New" keyword was use is because whenever the their are new input then
+    //It will automatically create a new body for the Register
+    const newRegister = new Register(req.body);
+
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getPaginatedRegister: async (req, res) => {},
+  updateRegister: async (req, res) => {},
+  deleteRegister: async (req, res) => {},
 };
