2c536427843617473d3b64d460f2755fec23788e
Leovison <cleovison@gmail.com>
2024-04-18 19:38:28 +0800

    .

--- src/controllers/RegistererController.mjs
+++ src/controllers/RegistererController.mjs
@@ -2,22 +2,26 @@ import Register from "../model/User/Register.mjs";
 
 export const RegisterController = {
   createRegister: async (req, res) => {
-    //The reason why the "New" keyword was use is because whenever the their are new input then
-    //It will automatically create a new body for the Register
-    const newRegister = new Register(req.body);
-    //Then the user input will be save in the database
-    try {
-    const savedRegister = await newRegister.save();
-    
-    //If the Register is sucessful, send back the 201 status code and post body
-    if(savedRegister) res.status(201).send(newRegister)
-    } catch (err) {
-      console.log(err);
+    //First i must declare a variable in order to store the new data that has been
+    //Created, Secondly the new Register called because inorder to create a new Data
+    //You need the "New" keyword and then requesting all the Schema in the "Register"
+    //The "Req.Body" specify that i am requesting the whole body in the Register
+    const savedRegister = new Register(req.body);
+
+    try{
+      //First declare a new variable to call the "savedRegister",
+      //"The Await" keyword has been use and call the "savedRegister" so while
+      //We are waiting for the client 
+      const newRegister = await savedRegister.save()
+      if(newRegister) res.status(201).send(savedRegister)
+
+    }catch(error){
+      res.status(404).send(error)
     }
   },
 
   getPaginatedRegister: async (req, res) => {},
-  getRegisteredID: async(req,res) =>{},
+  getNewRegisterID: async (req, res) => {},
   updateRegister: async (req, res) => {},
   deleteRegister: async (req, res) => {},
 };
