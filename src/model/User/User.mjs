f9db832a39ed2a4911e8e318419d197b9a4e115e
Leovison <cleovison@gmail.com>
2024-05-02 18:30:27 +0800

    Added ByCryptJS, JSONWebToken & Authentication

--- src/model/User/User.mjs
+++ src/model/User/User.mjs
@@ -1,33 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  fname: {
-    type: String,
-    require: true,
-  },
-  lname: {
-    type: String,
-    require: true,
-  },
-  username: {
-    type: String,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  confirm: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-  },
-
-});
+  fname: { type: String, require: true },
+  lname: { type: String, require: true },
+  username: { type: String, require: true },
+  password: { type: String, require: true },
+  confirm: { type: String, require: true },
+  email: { type: String, require: true } });
 
 
-UserSchema.index({fname: 'text', lname: 'text', username: 'text', email: 'text'})
+  
+UserSchema.index({ fname: "text", lname: "text", username: "text", email: "text"});
 export default mongoose.model("User", UserSchema);
