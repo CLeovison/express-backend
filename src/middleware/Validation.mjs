e573d8d202b96a98e87e69d932f734a4b814762c
CLeovison <cleovison@gmail.com>
2024-05-05 00:06:18 +0800

    Register Validation Success

--- src/middleware/Validation.mjs
+++ src/middleware/Validation.mjs
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 //Schema Definition
+
 const queryValidation = Joi.object({
   limit: Joi.number().integer().min(1).default(10),
   page: Joi.number().integer().min(1).default(1),
@@ -8,17 +9,35 @@ const queryValidation = Joi.object({
   sortOrder: Joi.string().valid("asc", "desc").default("asc"),
 });
 
-const userValidation =  Joi.object({
-  fname: Joi.string()
-})
-
+const userValidation = Joi.object({
+  fname: Joi.string().required(),
+  lname: Joi.string().required(),
+  username: Joi.string().alphanum().min(8).max(30).required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  confirm: Joi.ref("password"),
+  email: Joi.string()
+    .email({ minDomainSegments: 3, tlds: { allow: ["com", "net"] } })
+    .required()
+});
 
 //Middleware Validation
 export const validationQuery = (req, res, next) => {
   const { error } = queryValidation.validate(req.query);
 
-  if(error){
-    return res.status(400).json({message: "Invalid query parameter", details: error.details })
+  if (error) {
+    return res
+      .status(400)
+      .json({ message: "Invalid query parameter", details: error.details });
   }
   next();
+};
+
+export const registerValidation = (req, res, next) => {
+  const { error } = userValidation.validate(req.body);
+
+  if (error) {
+    res
+      .status(404)
+      .json({ message: "Fill Up All The Forms", details: error.details });
+  }
 };
