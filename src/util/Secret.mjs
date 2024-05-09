54281653a4e00d71a47cbd898aa8edbe2ad5b021
Leovison <cleovison@gmail.com>
2024-05-09 18:12:34 +0800

    Added A Env File

--- src/util/Secret.mjs
+++ src/util/Secret.mjs
@@ -1,6 +1,10 @@
 import * as jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const secretToken = process.env.SECRET_KEY
 
 export const secretKey = async (req,res) =>{
-    
+
 }
