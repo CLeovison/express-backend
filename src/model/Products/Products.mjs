54032bf13ce29b10a79072111760efe400669f3d
Leovison <cleovison@gmail.com>
2024-05-15 11:23:16 +0800

    Modified The AuthRole & Added A Product Controller

--- src/model/Products/Products.mjs
+++ src/model/Products/Products.mjs
@@ -1,14 +1,14 @@
-import mongoose, { SchemaType } from "mongoose";
-
+import mongoose from "mongoose";
 
 export const ProductSchema = new mongoose.Schema({
-   available:{type: Boolean}
-
-
-})
+  available: { type: Boolean, required: true },
+  productname: { type: String, required: true },
+  producttype: { type: String, required: true },
+  productdetails: { type: String, required: true },
+  isSoftDelete: { type: Boolean, default: false },
+});
 
 // available: {type: Boolean}, productname:{type: String}, productype,productdetails, isSoftDelete
-
 
 //Wishlist userid, productid
 
