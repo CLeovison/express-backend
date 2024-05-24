aeed11a5247a8f2194c499e1adc356eb0657c606
Leovison <cleovison@gmail.com>
2024-05-24 10:21:38 +0800

    Changes

--- src/controllers/CartController.mjs
+++ src/controllers/CartController.mjs
@@ -1,7 +1,18 @@
 import { Cart } from "../model/Products/Cart.mjs";
 
 export const CartController = {
-  addCart: async (req, res) => {},
+  addCart: async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+      const savedCart = await newCart.save();
+
+      res.status(201).json({ message: "You Successfully Added An Item" });
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ error: "You didn't add an item", error });
+    }
+  },
   getAllCart: async (req, res) => {},
   updateCart: async (req, res) => {},
   deleteCart: async (req, res) => {},
