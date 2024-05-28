b1108c559a8eabc77ad8b9195be03f788110ef31
Leovison <cleovison@gmail.com>
2024-05-28 17:58:04 +0800

    Modifying The Mail

--- src/util/mail.mjs
+++ src/util/mail.mjs
@@ -3,7 +3,7 @@
     service: "gmail",
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,
+  
     auth: {
       user: gEmail,
       pass: myPassword, // Note That The Password That Needs to be in here is the password from app password in google
