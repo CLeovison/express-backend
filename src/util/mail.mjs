abff4cebd08b1b5c730d51eb3f17ef313835bfa3
CLeovison <cleovison@gmail.com>
2024-05-30 00:15:46 +0800

    App Password Done On Google

--- src/util/mail.mjs
+++ src/util/mail.mjs
@@ -1,28 +1,32 @@
-   //Nodemailer/Forgot Username Password
-   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-  
-    auth: {
-      user: gEmail,
-      pass: myPassword, // Note That The Password That Needs to be in here is the password from app password in google
-    },
-  });
+//Nodemailer/Forgot Username Password
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 
-  const mailOptions = {
-    from: "cleovison@gmail.com",
-    to: email,
-    subject: "Forgot Password",
-    text: "Please Reset your password",
-  };
+dotenv.config();
 
-  transporter.sendMail(mailOptions, (error, info) =>{
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.APP_PASSWORD, // Note That The Password That Needs to be in here is the password from app password in google
+  },
+});
 
-    if(error){
-      console.log(error);
-    }else{
-      console.log("Email sent: " + info.response);
-    }
-    return res.status(200).json({message: "Please Reset Your Password"})
-  })
+const mailOptions = {
+  from: "cleovison@gmail.com",
+  to: email,
+  subject: "Forgot Password",
+  text: "Please Reset your password",
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+  return res.status(200).json({ message: "Please Reset Your Password" });
+});
