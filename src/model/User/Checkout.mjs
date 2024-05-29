237d3166ea29a53484dccece85467203fdbd6297
Leovison <cleovison@gmail.com>
2024-05-29 18:08:00 +0800

    Modifying the Checkout Model

--- src/model/User/Checkout.mjs
+++ src/model/User/Checkout.mjs
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 export const CheckoutSchema = new mongoose.Schema({
-    fname: {type: String, required: true},
-    lname: {type: String, required: true},
-    address:{type: String, required: true},
-    city: {type: String,}
-})
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  postal: { type: Number, required: true },
+});
+
+
+export default mongoose.model('Checkout', CheckoutSchema)
