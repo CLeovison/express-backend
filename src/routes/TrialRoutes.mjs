de606e7ec1882c0a34c3ac4c196d3ba6201a4fec
CLeovison <cleovison@gmail.com>
2024-06-02 19:54:25 +0800

    Remove EJS

--- src/routes/TrialRoutes.mjs
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { TrialController } from "../controllers/TrialController.mjs";
-export const TrialRouter = Router();
-
-TrialRouter.get("/trial/forgot-password", TrialController.forgotPassword)
-TrialRouter.get("/trial/reset-password/:id/:token", TrialController.resetPass)
