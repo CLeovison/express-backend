409f98586c62429a261067878b958d22ccc8dc35
Clark Leovison Rey <cleovison@gmail.com>
2024-08-11 17:26:53 +0800

    Added Multer

--- src/routes/ProductRoutes.mjs
+++ src/routes/ProductRoutes.mjs
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { ProductController } from "../controllers/ProductController.mjs";
-
+import multer from "multer";
 export const ProductRouter = Router();
+const upload = multer();
 
 ProductRouter.post("/products/create", ProductController.createProduct);
 ProductRouter.get("/products/", ProductController.paginatedProducts);
