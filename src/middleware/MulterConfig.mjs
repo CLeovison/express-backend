062504919c1b112e5e765901be3acc05c4fe1292
Clark Leovison Rey <cleovison@gmail.com>
2024-08-13 00:08:43 +0800

    Remove the console log

--- src/middleware/MulterConfig.mjs
+++ src/middleware/MulterConfig.mjs
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 
 import { cwd } from "node:process";
-console.log(cwd())
+
 export const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, cwd() + "/uploads/");
